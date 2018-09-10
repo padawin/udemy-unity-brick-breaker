a1ead4b890c8306348818409e8ca2cc71b1db459
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-10 19:10:34 +0100

    First scenes added

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+	public void loadNextScene() {
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex + 1);
+	}
+
+	public void loadFirstScene() {
+		SceneManager.LoadScene(0);
+	}
+
+	public void loadScene(int index) {
+		SceneManager.LoadScene(index);
+	}
+
+	public void quit() {
+		Application.Quit();
+	}
+}
