d60e1d86e66a765363b0bf6bdb23735b8b038315
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-12 20:05:10 +0100

    Make ball follow the paddle at the beginning

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	[SerializeField] Paddle paddle;
+
+	Vector2 distanceFromPaddle;
+
+	// Use this for initialization
+	void Start () {
+		distanceFromPaddle = transform.position - paddle.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 newPos = new Vector2(
+			paddle.transform.position.x,
+			paddle.transform.position.y
+		);
+		transform.position = newPos + distanceFromPaddle;
+	}
+}
