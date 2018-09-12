1bf3c6bbafdd9854a98236f2cef5078ced63b084
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-12 13:17:48 +0100

    collisions added and paddle animated

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+	[SerializeField] float screenWidthInUnits = 16f;
+	[SerializeField] float minPosX = 1;
+	[SerializeField] float maxPosX = 16;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float newX = Mathf.Clamp(
+			Input.mousePosition.x / Screen.width * screenWidthInUnits,
+			minPosX, maxPosX
+		);
+		Vector2 newPos = new Vector2(newX, transform.position.y);
+		transform.position = newPos;
+	}
+}
