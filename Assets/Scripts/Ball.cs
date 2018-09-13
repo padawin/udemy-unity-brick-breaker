b2e86b385afa17de348be2a2ce44184b61eb04e7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-13 19:17:04 +0100

    Ball started on click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,8 +2,10 @@
 
 public class Ball : MonoBehaviour {
 	[SerializeField] Paddle paddle;
+	[SerializeField] float speed = 5f;
 
 	Vector2 distanceFromPaddle;
+	bool isLaunched = false;
 	// Use this for initialization
 	void Start () {
 		distanceFromPaddle = transform.position - paddle.transform.position;
@@ -17,8 +19,28 @@ public class Ball : MonoBehaviour {
 		transform.position = newPos + distanceFromPaddle;
 	}
 
+	void launch() {
+		float initialAngle = Random.Range(Mathf.PI / 6, 5 * Mathf.PI / 6);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(
+			speed * Mathf.Cos(initialAngle),
+			speed * Mathf.Sin(initialAngle)
+		);
+	}
+
 	// Update is called once per frame
-	void Update () {
-		lockOnPaddle();
+	void Update() {
+		if (!isLaunched) {
+			lockOnPaddle();
+			if (Input.GetMouseButtonDown(0))
+			{
+				launch();
+				isLaunched = true;
+			}
+		}
+		else {
+			Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+			velocity.Normalize();
+			GetComponent<Rigidbody2D>().velocity = velocity * speed;
+		}
 	}
 }
