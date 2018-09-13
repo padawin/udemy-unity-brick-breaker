d484c3065e8f143b4889dac10f203551d60acc4e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-13 18:53:03 +0100

    private method refactored

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,18 +4,21 @@ public class Ball : MonoBehaviour {
 	[SerializeField] Paddle paddle;
 
 	Vector2 distanceFromPaddle;
-
 	// Use this for initialization
 	void Start () {
 		distanceFromPaddle = transform.position - paddle.transform.position;
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void lockOnPaddle() {
 		Vector2 newPos = new Vector2(
 			paddle.transform.position.x,
 			paddle.transform.position.y
 		);
 		transform.position = newPos + distanceFromPaddle;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		lockOnPaddle();
 	}
 }
