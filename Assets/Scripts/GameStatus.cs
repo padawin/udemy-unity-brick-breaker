05ef0fa56d3aa62a1a4ed39f540a2e3396d1e005
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-15 10:56:36 +0100

    Game status added to adjust the game speed

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GameStatus : MonoBehaviour {
+	[SerializeField] [Range(0f, 3f)] float gameSpeed = 1f;
+
+	// Update is called once per frame
+	void Update () {
+		Time.timeScale = gameSpeed;
+	}
+}
