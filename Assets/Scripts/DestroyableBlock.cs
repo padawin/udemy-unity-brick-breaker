5583a75dce15d175aa59776ed46ebddb089b8e44
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-16 13:35:31 +0100

    Particles added when destroying a block

--- Assets/Scripts/DestroyableBlock.cs
+++ Assets/Scripts/DestroyableBlock.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 
 public class DestroyableBlock : _Brick {
+	[SerializeField] GameObject destroySparkles;
+
 	private new void OnCollisionEnter2D(Collision2D collision) {
 		base.OnCollisionEnter2D(collision);
 		remove(0f);
+		triggerSparkles();
+	}
+
+	private void triggerSparkles() {
+		GameObject sparkles = Instantiate(destroySparkles, transform.position, transform.rotation);
+		Destroy(sparkles, 1f);
 	}
 }
