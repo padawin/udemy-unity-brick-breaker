ab395ce4031d1568dcf46bc61d2fe98714e05fae
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-19 20:13:22 +0100

    Last mission - Death star added

---
+++ Assets/Scripts/ExplodingBlock.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ExplodingBlock : _Brick {
+	[SerializeField] Explosion explosion;
+
+	private new void OnCollisionEnter2D(Collision2D collision) {
+		base.OnCollisionEnter2D(collision);
+		Instantiate(explosion, transform.position, transform.rotation);
+	}
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+	[SerializeField] float duration = 5f;
+	[SerializeField] float expansionSpeed = .1f;
+	[SerializeField] Color colorFrom;
+	[SerializeField] Color colorTo;
+
+	float timeBirth;
+	Vector2 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+		timeBirth = Time.time;
+		transform.localScale = new Vector2(0, 0);
+		colorFrom.a = 1f;
+		colorTo.a = 1f;
+		GetComponent<SpriteRenderer>().color = colorFrom;
+		Destroy(gameObject, duration);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = initialPosition;
+		float progress = (Time.time - timeBirth) / duration;
+		transform.localScale = new Vector2(
+			transform.localScale.x + expansionSpeed,
+			transform.localScale.y + expansionSpeed
+		);
+		Color c = new Color(
+			Mathf.Lerp(colorFrom.r, colorTo.r, progress),
+			Mathf.Lerp(colorFrom.g, colorTo.g, progress),
+			Mathf.Lerp(colorFrom.b, colorTo.b, progress),
+			1f
+		);
+		GetComponent<SpriteRenderer>().color = c;
+	}
+}
